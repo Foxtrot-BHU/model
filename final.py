630fa130e5ee65a5c81c117516602fb4ed41fcd2
horrifyingHorse <excuseplanation@gmail.com>
2025-03-10 11:58:31 +0530

    expose REST endpoints

--- final.py
+++ final.py
@@ -4,12 +4,85 @@ import os
 
 import google.generativeai as genai
 import pdfplumber
+import uvicorn
 from dotenv import load_dotenv
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse, JSONResponse
+from google.ai.generativelanguage_v1beta.types import content
 
 _ = load_dotenv()
-
 model = genai.GenerativeModel("gemini-1.5-flash")
 genai.configure(api_key=os.getenv("LEAKED_API_KEY"))
+app = FastAPI()
+wsocks: list[int] = []
+wsocks_metadata = {}
+
+
+@app.get("/")
+async def root():
+    return HTMLResponse("<p style='padding-left: 2rem;'>listenin</p>")
+
+
+@app.get("/extract")
+async def extractEndpoint(id: int, jd: str):
+    if id in wsocks:
+        return JSONResponse(
+            status_code=200,
+            content={"id": id, "JD": f"{jd}"},
+        )
+    wsocks.append(id)
+    wsocks_metadata[id] = jd
+    return JSONResponse(
+        status_code=200,
+        content={"id": id, "JD": f"{jd}"},
+    )
+
+
+@app.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+async def catch_all(full_path: str):
+    return JSONResponse(
+        status_code=404,
+        content={"detail": f"Route '/{full_path}' not found"},
+    )
+
+
+@app.websocket("/extract/{id}")
+async def websocket_rank(websocket: WebSocket, id: int):
+    await websocket.accept()
+    if id not in wsocks:
+        await websocket.send_json({"error": "Invalid ID"})
+        await websocket.close()
+        return
+    await websocket.send_json(
+        {"status": "Connected", "message": "Extraction started..."}
+    )
+
+    jd = wsocks_metadata[id]
+    files = [
+        "./uploads/Ashish_Resume_ATS.pdf",
+        "./uploads/Vienna-Modern-Resume-Template.pdf",
+        "./uploads/Dublin-Resume-Template-Modern.pdf",
+        "./uploads/Sydney-Resume-Template-Modern.pdf",
+        "./uploads/Amsterdam-Modern-Resume-Template.pdf",
+        "./uploads/Stockholm-Resume-Template-Simple.pdf",
+        "./uploads/London-Resume-Template-Professional.pdf",
+    ]
+    await websocket.send_json({"status": "Connected", "message": "Extracting Text..."})
+    print("Extracting text", flush=True)
+    extracted_texts = await asyncio.gather(*[extract(file) for file in files])
+
+    await websocket.send_json({"status": "Connected", "message": "Gathering..."})
+    print("Gathering...", flush=True)
+    response = await asyncio.gather(*[getResponse(text) for text in extracted_texts])
+    print(response)
+
+    await websocket.send_json({"status": "Ranking started", "message": None})
+
+    await websocket.send_json({"status": "Complete", "message": None})
+    await websocket.close()
+
+    del wsocks_metadata[id]
+    wsocks.remove(id)
 
 
 async def extract(pdf_path: str) -> str:
@@ -44,25 +117,9 @@ def getPrompt(resume_text: str) -> str:
 
 async def getResponse(prompt: str):
     response = await model.generate_content_async(getPrompt(prompt))
-    print(response.text, flush=True)
-    return response
+    # print(response.text, flush=True)
+    return response.text
 
 
-async def main():
-    files = [
-        "./uploads/Ashish_Resume_ATS.pdf",
-        "./uploads/Vienna-Modern-Resume-Template.pdf",
-        "./uploads/Dublin-Resume-Template-Modern.pdf",
-        "./uploads/Sydney-Resume-Template-Modern.pdf",
-        "./uploads/Amsterdam-Modern-Resume-Template.pdf",
-        "./uploads/Stockholm-Resume-Template-Simple.pdf",
-        "./uploads/London-Resume-Template-Professional.pdf",
-    ]
-    print("Extracting text", flush=True)
-    extracted_texts = await asyncio.gather(*[extract(file) for file in files])
-
-    print("Gathering...", flush=True)
-    _ = await asyncio.gather(*[getResponse(text) for text in extracted_texts])
-
-
-asyncio.run(main())
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
